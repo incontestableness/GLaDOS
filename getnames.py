650bc2cd1c9f38c4d24c9a5747693c9885719a05
incontestableness <whyistherumg0ne@protonmail.com>
2021-05-11 23:36:19 -0500

    Update getnames.py

--- getnames.py
+++ getnames.py
@@ -2,16 +2,25 @@
 
 # Quick script to print bot names from the API in name_blacklist.txt format
 
+import argparse
 import re
 import requests
 
 
 
-names = []
+parser = argparse.ArgumentParser()
+parser.add_argument("--no-count", action="store_true")
+args = parser.parse_args()
+
+
+botnames = []
 response = requests.get("http://milenko.ml/api/botnames")
 for i in response.json()["response"]["bot_names"]:
-	names.append([re.escape(i["name"]).encode("unicode-escape").decode(), i["properties"]["times_seen"]])
-names.sort()
-for i in names:
-	if i[1] > 10:
-		print(i[0])
+	botnames.append([re.escape(i["name"]).replace("\\ ", " ").encode("unicode-escape").decode(), i["properties"]["times_seen"]])
+
+botnames.sort()
+for name, count in botnames:
+	if args.no_count:
+		print(name)
+	else:
+		print(name.ljust(160), count)
