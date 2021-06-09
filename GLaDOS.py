f9a09276a54b56a1142ba0738c23af334c46240d
incontestableness <whyistherumg0ne@protonmail.com>
2021-06-09 14:52:39 -0500

    Dynamic times seen requirements based on scanning speed

--- GLaDOS.py
+++ GLaDOS.py
@@ -26,9 +26,8 @@ from valve_whitelist import cidrs
 parser = argparse.ArgumentParser()
 # You may wish to tune this based on your ping to the servers
 parser.add_argument("--scan-timeout", type=float, default=0.500)
-# Must align with scanning speed, see namerules() and MoralityCore.check_name()
-parser.add_argument("--suspicious-times-seen", type=int, default=720)
-parser.add_argument("--cheater-times-seen", type=int, default=2160)
+# How long it takes to complete a scan, in seconds. Affects times seen requirements.
+parser.add_argument("--scan-duration", type=float, default=3.0)
 args = parser.parse_args()
 
 
@@ -42,6 +41,14 @@ logger = logging.getLogger()
 print = logger.info
 
 
+# Calculate appropriate times seen requirements based on scanning speed
+# Reasonably confident. For a false positive, a single user with a name starting with (N) would have to be in-game for the full course of an hour.
+suspicious_times_seen = int((60 * 60) / args.scan_duration)
+# Very high confidence. For a false positive, a single user with a name starting with (N) would have to be in-game for the full course of 3 hours.
+cheater_times_seen = int((60 * 60 * 3) / args.scan_duration)
+print(f"Times seen requirements: {suspicious_times_seen} for suspicious, {cheater_times_seen} for cheater")
+
+
 # Read in regions.json for nicer stats output
 file = open("regions.json", "r")
 data = file.read()
@@ -50,7 +57,7 @@ regions_data = json.loads(data)
 
 
 # Create a GLaDOS core
-core = MoralityCore(args.scan_timeout, args.suspicious_times_seen, args.cheater_times_seen)
+core = MoralityCore(args.scan_timeout, suspicious_times_seen, cheater_times_seen)
 
 
 # Declare our Flask instance and define all the API routes
@@ -208,12 +215,9 @@ def namerules():
 		# Ignore names older than 24h
 		if time.time() - pn.last_seen >= 60 * 60 * 24:
 			continue
-		# Scans run about every 5 seconds. This is taken into account.
-		# Very high confidence. For a false positive, a single user with a name starting with (N) would have to be in-game for the full course of 3 hours.
-		if pn.times_seen >= args.cheater_times_seen:
+		if pn.times_seen >= cheater_times_seen:
 			bnames.append(pn)
-		# Reasonably confident. For a false positive, a single user with a name starting with (N) would have to be in-game for the full course of an hour.
-		elif pn.times_seen >= args.suspicious_times_seen:
+		elif pn.times_seen >= suspicious_times_seen:
 			snames.append(pn)
 	data = {"$schema": "https://raw.githubusercontent.com/PazerOP/tf2_bot_detector/master/schemas/v3/rules.schema.json"}
 	data["file_info"] = {
